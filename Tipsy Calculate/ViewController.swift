e87e805b854692d8338adc59f615ed23c1f458bc
Meddor12 <Ergashov5666@gmail.com>
2023-02-03 16:33:17 +0300

    Initial Commit

---
+++ Tipsy Calculate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy Calculate
+//
+//  Created by Meddor on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
